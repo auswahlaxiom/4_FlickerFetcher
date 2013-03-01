b5afa3abd7ba0f059107f4002ad81da47c3d83ed
Zach Fleischman <zfleisch@mines.edu>
2013-02-28 23:35:00 -0700

    set up table and tab view controller

--- 4_FlickerFetcher/TopPlacesTVC.h
+++ 4_FlickerFetcher/TopPlacesTVC.h
@@ -1,5 +1,5 @@
 //
-//  FlickerFetcherViewController.h
+//  TopPlacesTVC.h
 //  4_FlickerFetcher
 //
 //  Created by Zachary Fleischman on 2/28/13.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FlickerFetcherViewController : UIViewController
+@interface TopPlacesTVC : UITableViewController
 
 @end
